f6bd5f91e7dec5bd6fda18dfadab29a2f81f248b
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2024-04-28 12:49:39 +0700

    fail

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const { notFound, erroHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json({ extends: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "https://alqudsi.vercel.app/" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use("/galeri", express.static(__dirname + "/galeri"));
@@ -29,7 +29,7 @@ app.use(notFound);
 app.use(erroHandler);
 
 connect(process.env.database)
-  .then(app.listen("alqudsi-api.vercel.app", () => console.log(`Server on port ${process.env.PORT}`)))
+  .then(app.listen(process.env.PORT || 5000, () => console.log(`Server on port ${process.env.PORT}`)))
   .catch((error) => {
     console.log(error);
   });
