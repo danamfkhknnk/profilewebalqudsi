760d7cb5effdf14483568c9a110d5cc7f7b00eb0
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2024-04-29 09:35:29 +0700

    done hosting backend to vercel

--- backend/routes/usersRouters.js
+++ backend/routes/usersRouters.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
 
 const { loginUser, registerUser } = require("../controllers/userController");
 const router = Router();
